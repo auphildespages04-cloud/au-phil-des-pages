1df3d654962f0de3f59965e1e8a80d9090fa0d62
auphildespages04-cloud <auphildespages04@gmail.com>
2026-04-23 11:27:26 +0200

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:40, fontFamily:"Arial"}}>
-      <h1>🚧 Site en cours de transformation...</h1>
-      <p>La nouvelle version fun arrive dans quelques minutes 😄</p>
-    </main>
-  );
-}
